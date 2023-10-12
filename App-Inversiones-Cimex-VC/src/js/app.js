ae652512b505c5009e13b5220f2deea388f9456d
AliaxDev <estupi1968@gmail.com>
2023-10-12 00:18:38 -0400

    update

--- App-Inversiones-Cimex-VC/src/js/app.js
+++ App-Inversiones-Cimex-VC/src/js/app.js
@@ -1,7 +1,3 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
 // select span
@@ -14,7 +10,6 @@ const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
-  // linksContainer.classList.toggle("show-links");
 
   const linksHeight = links.getBoundingClientRect().height;
   const containerHeight = linksContainer.getBoundingClientRect().height;
@@ -38,11 +33,9 @@ window.addEventListener("scroll", function () {
   } else {
     navbar.classList.remove("fixed-nav");
   }
+  
   // setup back to top link
-
   if (scrollHeight > 500) {
-    //console.log("hello");
-
     topLink.classList.add("show-link");
   } else {
     topLink.classList.remove("show-link");
@@ -81,28 +74,3 @@ scrollLinks.forEach((link) => {
   });
 });
 // calculate heights
-
-const testiomnialData = [
-  "https://cdn.pixabay.com/photo/2016/08/01/15/47/music-1561355_640.jpg",
-  "https://cdn.pixabay.com/photo/2020/05/17/17/11/bell-5182677_640.jpg",
-  "https://cdn.pixabay.com/photo/2023/05/20/09/31/ai-generated-8006192_640.jpg",
-  ]
-  const slideHolder = document.querySelector("#slideHolder")
-  for (let i of testiomnialData) slideHolder.innerHTML += "<div class='swiper-slide'><img src="+i+"></div>"
-  const swiper = new Swiper('.MainSlider', {
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 1,
-      loop: true,
-      spaceBetween: 30,
-      pagination: {
-          el: '.swiper-pagination',
-          clickable: true
-      },
-      autoplay: { delay: 5000 }
-  });
-  const Cateogryswiper = new Swiper(".categories", {
-      slidesPerView:"auto",
-      spaceBetween: 10,
-      freeMode: true,
-    });
