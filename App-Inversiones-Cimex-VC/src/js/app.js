028a4e4ba4e3fde0184072c2f0fbd106862a7d74
AliaxDev <estupi1968@gmail.com>
2023-10-22 16:22:10 -0400

    update 22-10-23

--- App-Inversiones-Cimex-VC/src/js/app.js
+++ App-Inversiones-Cimex-VC/src/js/app.js
@@ -42,7 +42,7 @@ window.addEventListener("scroll", function () {
   // setup back to top link
 
   if (scrollHeight > 500) {
-    console.log("helo");
+    //console.log("scroll");
 
     topLink.classList.add("show-link");
   } else {
--- App-Inversiones-Cimex-VC/src/js/script-form.js
+++ App-Inversiones-Cimex-VC/src/js/script-form.js
@@ -1,43 +1,49 @@
-$("#contactform").validate({
-    rules: {
-        name: "required",
-        email: "required",
-        phone: {
-            required: true,
-            minlength: 10,
-            number: true
+const form = document.querySelector('form');
+const formMessage = document.querySelector('.form-message');
+
+async function handleSubmit(event) {
+
+    event.preventDefault();
+
+    var data = new FormData(event.target);
+    fetch('https://formspree.io/f/mvojlzlo', {
+        method: 'POST',
+        body: data,
+        headers: {
+            'Accept': 'application/json'
         }
-    },
-    message: {
-        name: "Please enter your name",
-        email: "Please enter your email id",
-        phone: {
-            required: "Please enter mobile no.",
-            minlength: "Please enter valid mobile no."
+    }).then(response => {
+
+        if (response.ok) {
+            
+            formMessage.innerHTML = "<span>Enviado con EXITO!</span>...Gracias por Escribirnos!";
+            formMessage.classList.toggle('hiden')
+            console.log("success")
+            form.reset()
+
+        } else {
+
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+
+                    let err = data["errors"].map(error => error["message"]).join(", ");
+
+                    if(err == "should be an email"){
+                        formMessage.innerHTML = `<span>Escriba un Email Valido</span>`;
+                    }else {
+                        formMessage.innerHTML = `<span>${err}</span>`;
+                    }
+            
+                } else {
+
+                    formMessage.innerHTML = "Oops! Ha Habido un Problema!"
+                }
+            })
         }
-    },
-    /* 
-    submitHandler: function() {
-        $.ajax({
-            url: 'sendform.js',
-            data: $('#contactform').serialize(),
-            type: 'POST',
-            beforeSend: function() {
-                $("#loading-image").show();
-                },
-            success: function(data) {
-                //alert('mail send');
-             $("#loading-image").hide();  
-             $("#success").show().fadeIn(1000);
-             $('#contactform').each(function() {
-              this.reset()
-             })
-            },
-            error: function(data) {
-                alert('error send');
-             $("#error").show().fadeIn(1000)
-            }
-           })
-    }
-    */
-});
+
+    }).catch(error => {
+        formMessage.innerHTML = "Oops! Ha Habido un Problema!"
+    });
+}
+
+form.addEventListener("submit", handleSubmit)
